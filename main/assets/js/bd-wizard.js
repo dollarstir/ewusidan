777bf0954da1213b9ec37467febafb9342ea452c
dollarstir <kpin463@gmail.com>
2022-04-13 07:02:34 +0000

    ssascssa

--- main/assets/js/bd-wizard.js
+++ main/assets/js/bd-wizard.js
@@ -1,43 +1,82 @@
-//Wizard Init
 
-$("#wizard").steps({
-    headerTag: "h3",
-    bodyTag: "section",
-    transitionEffect: "none",
-    titleTemplate: '#title#'
+//jQuery time
+var current_fs, next_fs, previous_fs; //fieldsets
+var left, opacity, scale; //fieldset properties which we will animate
+var animating; //flag to prevent quick multi-click glitches
+
+$(".next").click(function(){
+	if(animating) return false;
+	animating = true;
+	
+	current_fs = $(this).parent();
+	next_fs = $(this).parent().next();
+	
+	//activate next step on progressbar using the index of next_fs
+	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+	
+	//show the next fieldset
+	next_fs.show(); 
+	//hide the current fieldset with style
+	current_fs.animate({opacity: 0}, {
+		step: function(now, mx) {
+			//as the opacity of current_fs reduces to 0 - stored in "now"
+			//1. scale current_fs down to 80%
+			scale = 1 - (1 - now) * 0.2;
+			//2. bring next_fs from the right(50%)
+			left = (now * 50)+"%";
+			//3. increase opacity of next_fs to 1 as it moves in
+			opacity = 1 - now;
+			current_fs.css({
+        'transform': 'scale('+scale+')',
+        'position': 'absolute'
+      });
+			next_fs.css({'left': left, 'opacity': opacity});
+		}, 
+		duration: 800, 
+		complete: function(){
+			current_fs.hide();
+			animating = false;
+		}, 
+		//this comes from the custom easing plugin
+		easing: 'easeInOutBack'
+	});
 });
 
-//Form control
-
-$('#firstName').on('change', function(e) {
-    $('#enteredFirstName').text(e.target.value || 'Cha');
+$(".previous").click(function(){
+	if(animating) return false;
+	animating = true;
+	
+	current_fs = $(this).parent();
+	previous_fs = $(this).parent().prev();
+	
+	//de-activate current step on progressbar
+	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+	
+	//show the previous fieldset
+	previous_fs.show(); 
+	//hide the current fieldset with style
+	current_fs.animate({opacity: 0}, {
+		step: function(now, mx) {
+			//as the opacity of current_fs reduces to 0 - stored in "now"
+			//1. scale previous_fs from 80% to 100%
+			scale = 0.8 + (1 - now) * 0.2;
+			//2. take current_fs to the right(50%) - from 0%
+			left = ((1-now) * 50)+"%";
+			//3. increase opacity of previous_fs to 1 as it moves in
+			opacity = 1 - now;
+			current_fs.css({'left': left});
+			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+		}, 
+		duration: 800, 
+		complete: function(){
+			current_fs.hide();
+			animating = false;
+		}, 
+		//this comes from the custom easing plugin
+		easing: 'easeInOutBack'
+	});
 });
 
-$('#lastName').on('change', function(e) {
-    $('#enteredLastName').text(e.target.value || 'Ji-Hun C');
-});
-
-$('#phoneNumber').on('change', function(e) {
-    $('#enteredPhoneNumber').text(e.target.value || '+230-582-6609');
-});
-
-$('#emailAddress').on('change', function(e) {
-    $('#enteredEmailAddress').text(e.target.value || 'willms_abby@gmail.com');
-});
-
-$('#designation').on('change', function(e) {
-    $('#enteredDesignation').text(e.target.value || 'Junior Developer');
-});
-
-$('#department').on('change', function(e) {
-    $('#enteredDepartment').text(e.target.value || 'UI Development');
-});
-
-$('#employeeNumber').on('change', function(e) {
-    $('#enteredEmployeeNumber').text(e.target.value || 'JDUI36849');
-});
-
-$('#workEmailAddress').on('change', function(e) {
-    $('#enteredWorkEmailAddress').text(e.target.value || 'willms_abby@company.com');
-});
-
+$(".submit").click(function(){
+	$('#msform').submit();
+})
