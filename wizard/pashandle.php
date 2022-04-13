8c47132baae025a02267b4c6693a052b2823d7c1
dollarstir <kpin463@gmail.com>
2022-04-13 22:05:17 +0000

    dass

--- wizard/pashandle.php
+++ wizard/pashandle.php
@@ -3,4 +3,332 @@
 if (isset($_POST['requestid'])) {
     $rid = $_POST['requestid'];
     extract($_POST);
+
+    $message = '<table style="width:100%;border-collapse:collapse;" >
+
+                <tr>
+                <th style="border-collapse:collapse;">Question</th>
+                <th style="border-collapse:collapse;">Answers</th>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+
+                <tr>
+                <td></td>
+                <td>'.$.'</td>
+                </tr>
+    
+        </table>';
+
+    switch ($rid) {
+        case '1':
+    }
 }
