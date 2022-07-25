4f3c7a58469be909c0c9ff568ff02d9cc7be86d9
dollarstir <kpin463@gmail.com>
2022-07-25 11:56:56 +0000

    sdfsd

--- backend/viewUsers.php
+++ backend/viewUsers.php
@@ -1,30 +1,8 @@
-<!DOCTYPE html>
-<html lang="zxx">
+<?php
 
-<head>
-
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <title>Sales</title>
-
-
-    <link rel="stylesheet" href="css/bootstrap1.min.css" />
-
-    <link rel="stylesheet" href="vendors/themefy_icon/themify-icons.css" />
-
-    <link rel="stylesheet" href="vendors/scroll/scrollable.css" />
-
-    <link rel="stylesheet" href="vendors/font_awesome/css/all.min.css" />
-
-    <link rel="stylesheet" href="vendors/datatable/css/jquery.dataTables.min.css" />
-    <link rel="stylesheet" href="vendors/datatable/css/responsive.dataTables.min.css" />
-    <link rel="stylesheet" href="vendors/datatable/css/buttons.dataTables.min.css" />
-
-
-    <link rel="stylesheet" href="css/metisMenu.css">
-
-    <link rel="stylesheet" href="css/style1.css" />
-</head>
+involve('admin');
+begin('Users');
+?>
 
 <body class="crm_body_bg">
 
@@ -35,7 +13,7 @@
                 <i class="ti-close"></i>
             </div>
         </div>
-        <?php include "sidebar.php"; ?>
+        <?php include 'sidebar.php'; ?>
     </nav>
 
     <section class="main_content dashboard_part large_header_bg">
--- fragement/admin.php
+++ fragement/admin.php
@@ -1,5 +1,32 @@
 <?php
 
-function adminhead($title)
+function begin($title)
 {
+    echo '<!DOCTYPE html>
+    <html lang="zxx">
+    
+    <head>
+    
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <title>'.$title.'</title>
+    
+    
+        <link rel="stylesheet" href="css/bootstrap1.min.css" />
+    
+        <link rel="stylesheet" href="vendors/themefy_icon/themify-icons.css" />
+    
+        <link rel="stylesheet" href="vendors/scroll/scrollable.css" />
+    
+        <link rel="stylesheet" href="vendors/font_awesome/css/all.min.css" />
+    
+        <link rel="stylesheet" href="vendors/datatable/css/jquery.dataTables.min.css" />
+        <link rel="stylesheet" href="vendors/datatable/css/responsive.dataTables.min.css" />
+        <link rel="stylesheet" href="vendors/datatable/css/buttons.dataTables.min.css" />
+    
+    
+        <link rel="stylesheet" href="css/metisMenu.css">
+    
+        <link rel="stylesheet" href="css/style1.css" />
+    </head>';
 }
