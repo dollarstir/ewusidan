7105563ca45ed90d83007f95b7ecda8a9ff0982a
dollarstir <kpin463@gmail.com>
2022-07-25 11:59:28 +0000

    dsfsd

--- fragement/admin.php
+++ fragement/admin.php
@@ -12,21 +12,21 @@ function begin($title)
         <title>'.$title.'</title>
     
     
-        <link rel="stylesheet" href="css/bootstrap1.min.css" />
+        <link rel="stylesheet" href="backend/css/bootstrap1.min.css" />
     
-        <link rel="stylesheet" href="vendors/themefy_icon/themify-icons.css" />
+        <link rel="stylesheet" href="backend/vendors/themefy_icon/themify-icons.css" />
     
-        <link rel="stylesheet" href="vendors/scroll/scrollable.css" />
+        <link rel="stylesheet" href="backend/vendors/scroll/scrollable.css" />
     
-        <link rel="stylesheet" href="vendors/font_awesome/css/all.min.css" />
+        <link rel="stylesheet" href="backend/vendors/font_awesome/css/all.min.css" />
     
-        <link rel="stylesheet" href="vendors/datatable/css/jquery.dataTables.min.css" />
-        <link rel="stylesheet" href="vendors/datatable/css/responsive.dataTables.min.css" />
-        <link rel="stylesheet" href="vendors/datatable/css/buttons.dataTables.min.css" />
+        <link rel="stylesheet" href="backend/vendors/datatable/css/jquery.dataTables.min.css" />
+        <link rel="stylesheet" href="backend/vendors/datatable/css/responsive.dataTables.min.css" />
+        <link rel="stylesheet" href="backend/vendors/datatable/css/buttons.dataTables.min.css" />
     
     
-        <link rel="stylesheet" href="css/metisMenu.css">
+        <link rel="stylesheet" href="backend/css/metisMenu.css">
     
-        <link rel="stylesheet" href="css/style1.css" />
+        <link rel="stylesheet" href="backend/css/style1.css" />
     </head>';
 }
