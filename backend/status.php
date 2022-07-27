82fa0cfbc5730ec69ec2907253a9250219135e06
dollarstir <kpin463@gmail.com>
2022-07-27 06:41:13 +0000

    fsdfsd

--- backend/status.php
+++ backend/status.php
@@ -1,16 +1,16 @@
 <?php
 
 $id = $_GET['token'];
-$c = customfetch('activity', [['id', '=', $id]]);
+$c = customfetch('activity', [['id', '=', $id]], ['id' => $id]);
 $cc = $c[0];
 switch ($cc['status']) {
     case 'pending':
-        $result = update('activity', ['status' => 'recieved']);
+        $result = update('activity', ['status' => 'recieved'], ['id' => $id]);
 
         break;
 
     case 'recieved':
-        $result = update('activity', ['status' => 'processed']);
+        $result = update('activity', ['status' => 'processed'], ['id' => $id]);
         break;
 
     default:
