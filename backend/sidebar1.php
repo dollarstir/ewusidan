1322b25ce9a5b2151a19b296641857bd68eb1d6a
dollarstir <kpin463@gmail.com>
2022-07-28 12:07:26 +0000

    aasdas

--- backend/sidebar1.php
+++ backend/sidebar1.php
@@ -22,7 +22,7 @@
             <span>Orders</span>
         </a>
         <ul>
-            <li><a class="active" href="neworders">New Orders <span class="badge badge-danger">1</span></a></li>
+            <li><a class="active" href="neworders">New Orders <span class="badge badge-danger">Danger</span></a></li>
             <li><a href="recievedorders">Recieved orders</a></li>
             <li><a href="completedorders">Processed orders</a></li>
             <li><a href="orders">All orders</a></li>
