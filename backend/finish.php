04c83b8d43ce2cffb1c91e0fe2a047965442bd0a
dollarstir <kpin463@gmail.com>
2022-07-30 01:30:25 +0000

    sfdsf

--- backend/finish.php
+++ backend/finish.php
@@ -159,6 +159,7 @@ begin('Pay For Service');
                         <div class="white_card card_height_100 mb_30">
 
                         <center><img src="main/assets/images/logo/logo.png" style="width:100px;height:100px;"/></center>
+                        <h3 class="m-0">Complete Your Application </h3>
                             <div class="white_card_header">
                                 <div class="box_header m-0">
                                     <div class="main-title">
