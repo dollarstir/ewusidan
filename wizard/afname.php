87d0d7fcc93d7565c654f11cf934689379a37fcc
dollarstir <kpin463@gmail.com>
2022-07-30 17:26:05 +0000

    jsdfd

--- wizard/afname.php
+++ wizard/afname.php
@@ -5,7 +5,7 @@ begin('AFFIDAVIT FOR CHANGE OF NAME ');
 <body>
 <div class="wrapper">
     <h3>Affidavit for change of name</h3>
-<form action="handler1" id="wizard" class="addpassport" method="post" enctype="multipart/form-data">
+<form  id="wizard" class="addaffidavitname" method="post" enctype="multipart/form-data">
 
 <h4></h4>
 <section>
