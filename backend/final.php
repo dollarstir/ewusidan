b494f887b12dd74a5f941e4610bd6e3f79590348
dollarstir <kpin463@gmail.com>
2022-07-30 09:41:42 +0000

    dfgdf

--- backend/final.php
+++ backend/final.php
@@ -5,7 +5,19 @@ if (!isset($_GET['ref'])) {
 } else {
     session_start();
     $orderid = $_SESSION['orderid'];
+    $amount = $_SESSION['servicefee'];
     $uid = $_SESSION['uid'];
-    customfetch('applicant', [['id', '=', $uid]]);
+    $user = customfetch('applicant', [['id', '=', $uid]]);
     update('activity', ['paystatus' => 'paid'], ['orderid' => $orderid]);
+    $message = '
+
+    <html>
+    <body>
+    <center><img src="http://ewusidanconsult.com/main/assets/images/logo/logo.png"/></center>
+    <p> Dear '.$user[0]['name'].', Your payment of GHS'.$amount.' has been recieved  </p>
+    </body>
+    </html>
+    
+    ';
+    sendmail('www.phpyolk.com', 'Payment Successful;', $message, 'Ewusidan Consult', ['kpin463@gmail.com', 'danielewusi2@gmail.com']);
 }
