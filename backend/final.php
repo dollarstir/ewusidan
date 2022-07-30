0326084b83027ca8a8d709b7b38454382711c0b8
dollarstir <kpin463@gmail.com>
2022-07-30 09:15:12 +0000

    ssdfds

--- backend/final.php
+++ backend/final.php
@@ -3,5 +3,9 @@
 if (!isset($_GET['ref'])) {
     echo'<script>window.location="home"</script>';
 } else {
-    echo 'all set';
+    session_start();
+    $orderid = $_SESSION['orderid'];
+    $uid = $_SESSION['uid'];
+    customfetch('applicant', [['id', '=', $uid]]);
+    update('activity', ['paystatus' => 'paid'], ['orderid' => $orderid]);
 }
